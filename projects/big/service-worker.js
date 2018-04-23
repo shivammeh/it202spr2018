2001154960cf3be442cdee5aaf5e48bf16e08006
Shivam Mehrotra <smehro4@uic.edu>
2018-04-23 02:50:19 +0000

    Trying to fix caching

--- projects/big/service-worker.js
+++ projects/big/service-worker.js
@@ -1,29 +1,29 @@
 var cacheName = 'big'; // big
 var dataCacheName = 'big-v1'; // big-v1
 var filesToCache = [
-  './',
-  './index.html',
-  './scripts/app.js',
-  './styles/inline.css',
+  '.',
+  'index.html',
+  'icon.png',
+  'background.jpeg',
+];
+var dataUrl = [
+  'https://maps.googleapis.com/maps/api/js'
 ];
 
 self.addEventListener('install', function(e) {
-  console.log('[ServiceWorker] Install');
   e.waitUntil(
     caches.open(cacheName).then(function(cache) {
-      console.log('[ServiceWorker] Caching app shell');
+      
       return cache.addAll(filesToCache);
     })
   );
 });
 
 self.addEventListener('activate', function(e) {
-  console.log('[ServiceWorker] Activate');
   e.waitUntil(
     caches.keys().then(function(keyList) {
       return Promise.all(keyList.map(function(key) {
         if (key !== cacheName && key !== dataCacheName) {
-          console.log('[ServiceWorker] Removing old cache', key);
           return caches.delete(key);
         }
       }));
@@ -32,32 +32,23 @@ self.addEventListener('activate', function(e) {
   return self.clients.claim();
 });
 
-
 self.addEventListener('fetch', function(e) {
-  console.log('[Service Worker] Fetch', e.request.url);
-  var dataUrl = 'https://query.yahooapis.com/v1/public/yql';
-  if (e.request.url.indexOf(dataUrl) > -1) {
-    /*
-     * When the request URL contains dataUrl, the app is asking for fresh
-     * weather data. In this case, the service worker always goes to the
-     * network and then caches the response. This is called the "Cache then
-     * network" strategy:
-     * https://jakearchibald.com/2014/offline-cookbook/#cache-then-network
-     */
+  var didMatchDataUrl = false;
+  for (var i = 0; i < dataUrls.length; i++)
+    if (e.request.url.indexOf(dataUrls[i]))
+      didMatchDataUrl = true;
+  if (didMatchDataUrl) {
     e.respondWith(
       caches.open(dataCacheName).then(function(cache) {
         return fetch(e.request).then(function(response){
           cache.put(e.request.url, response.clone());
           return response;
+        }).catch(function(event) {
+          return cache.match(e.request);
         });
       })
     );
   } else {
-    /*
-     * The app is asking for app shell files. In this scenario the app uses the
-     * "Cache, falling back to the network" offline strategy:
-     * https://jakearchibald.com/2014/offline-cookbook/#cache-falling-back-to-network
-     */
     e.respondWith(
       caches.match(e.request).then(function(response) {
         return response || fetch(e.request);
